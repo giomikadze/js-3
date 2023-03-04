fb307439bc955fcda7cee20c1abdda64d0de1d2f
giomikadze <110746808+giomikadze@users.noreply.github.com>
2023-03-04 13:17:45 +0100

    lesson-3

---
+++ lesson-js/script.js
@@ -0,0 +1,48 @@
+
+
+
+// let arr = {
+//   ru:['голубой', 'красный', 'зеленый'],
+//   en:['blue', 'red', 'green'],
+// };
+// console.log(arr.ru[0])
+
+
+// const obj = {
+//   kolya:200,
+//   vasya:300,
+//   petya:400,
+// };
+
+// for(let key in obj){
+//   console.log(`${key}: зарплата ${obj[key]} долларов!!`)
+// }
+
+
+
+// const obj = {
+//   js: ["jQuery", "Angular"],
+//   php: "hello",
+//   css: "world"
+// };
+// console.log(`${obj.js[0]}`);
+
+
+// const arr = {
+//   ru :[" Понедельник" ,"Вторник", "Среда","Четверг", "Пятница","Суббота","Воскресенье"],
+//   en :["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+// }
+// console.log(`${arr.ru[5]}`)
+// console.log(` ${arr.en[5]}`)
+
+
+// const days = {
+//   first: "Monday",
+//   second: "Tuesday",
+//   third: "Wednesday",
+//   fourth: "Thursday",
+//   fifth: "Friday",
+//   sixth: "Saturday",
+//   seventh: "Sunday"
+//   };
+// console.log(days.sixth);
